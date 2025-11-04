accd3c127cb5dd823b98243e6ced8e51e1fd6c8c
Jason Nikas <nikasjason1@gmail.com>
2025-11-04 23:05:36 +0200

    Add report and zip.sh script

---
+++ Lab2/zip.sh
@@ -0,0 +1,3 @@
+tar -czf NIKAS-IOANNIS-IASON_3771_TSOGKAS_PANAGIOTIS_3672_TSIANTOS_DIMITRIOS_3796.tar.gz \
+  --exclude='src/Image_data' \
+  src/ report.pdf plot.py run.sh testbench.sh .gitignore README
