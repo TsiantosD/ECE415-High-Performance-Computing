b9b53cb8660ba16d4415b51ba0893852ec85628a
Jason Nikas <nikasjason1@gmail.com>
2025-11-22 21:18:05 +0200

    Add initialization of kernel data, error checking and output checking

--- Lab3/src/step2/Convolution2D.cu
+++ Lab3/src/step2/Convolution2D.cu
@@ -8,6 +8,7 @@
 
 typedef enum {
     HOST_ALLOC = 1,
+    DEVICE_ERROR = 2,
     NORMAL = 0
 } ErrorCode;
 
@@ -20,14 +21,26 @@ ErrorCode exitCode = NORMAL;
 #define ABS(val)         ((val)<0.0 ? (-(val)) : (val))
 #define accuracy         0.00005 
 
-#define CHECK_ALLOC(ptr, label)        \
+#define CHECK_ALLOC_HOST(ptr)          \
     do {                               \
         if ((ptr) == NULL) {           \
             exitCode = HOST_ALLOC;     \
             printf("Allocation failed: %s\n", #ptr); \
-            goto label;                \
+            goto CLEANUP_HOST;         \
         }                              \
     } while(0)
+
+#define CUDA_CHECK_LAST_ERROR()                                              \
+    do {                                                                     \
+        cudaDeviceSynchronize();                                             \
+        cudaError_t _err = cudaGetLastError();                               \
+        if (_err != cudaSuccess) {                                           \
+            printf("CUDA Error: %s in %s, line %d\n",                        \
+                   cudaGetErrorString(_err), __FILE__, __LINE__);            \
+            exitCode = DEVICE_ERROR;                                         \
+            goto CLEANUP_DEVICE;                                             \
+        }                                                                    \
+    } while (0)
 
 ////////////////////////////////////////////////////////////////////////////////
 // Reference row convolution filter
@@ -81,6 +94,10 @@ void convolutionColumnCPU(PixelScalar *h_Dst, PixelScalar *h_Src, PixelScalar *h
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+// Kernel convolution filter
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
 // Main program
 ////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char **argv) {
@@ -89,6 +106,7 @@ int main(int argc, char **argv) {
         *h_Input = NULL,
         *h_Buffer = NULL,
         *h_OutputCPU = NULL,
+        *h_OutputGPU = NULL,
         *d_Filter = NULL,
         *d_Input = NULL,
         *d_Output = NULL;
@@ -96,6 +114,7 @@ int main(int argc, char **argv) {
     int imageW;
     int imageH;
     unsigned int i;
+    int correctOutput = 1;
   
     printf("Enter filter radius : ");
     scanf("%d", &filter_radius);
@@ -112,13 +131,25 @@ int main(int argc, char **argv) {
     printf("Allocating and initializing host arrays...\n");
 
     h_Filter    = (PixelScalar *)malloc(FILTER_LENGTH * sizeof(PixelScalar));
-    CHECK_ALLOC(h_Filter, CLEANUP_HOST);
+    CHECK_ALLOC_HOST(h_Filter);
     h_Input     = (PixelScalar *)malloc(imageW * imageH * sizeof(PixelScalar));
-    CHECK_ALLOC(h_Input, CLEANUP_HOST);
+    CHECK_ALLOC_HOST(h_Input);
     h_Buffer    = (PixelScalar *)malloc(imageW * imageH * sizeof(PixelScalar));
-    CHECK_ALLOC(h_Buffer, CLEANUP_HOST);
+    CHECK_ALLOC_HOST(h_Buffer);
     h_OutputCPU = (PixelScalar *)malloc(imageW * imageH * sizeof(PixelScalar));
-    CHECK_ALLOC(h_OutputCPU, CLEANUP_HOST);
+    CHECK_ALLOC_HOST(h_OutputCPU);
+    h_OutputGPU = (PixelScalar *)malloc(imageW * imageH * sizeof(PixelScalar));
+    CHECK_ALLOC_HOST(h_OutputGPU);
+    cudaMalloc((void **) &d_Filter, FILTER_LENGTH * sizeof(PixelScalar));
+    CUDA_CHECK_LAST_ERROR();
+    cudaMalloc((void **) &d_Input, imageW * imageH * sizeof(PixelScalar));
+    CUDA_CHECK_LAST_ERROR();
+    cudaMalloc((void **) &d_Output, imageW * imageH * sizeof(PixelScalar));
+    CUDA_CHECK_LAST_ERROR();
+    cudaMemcpy(d_Filter, h_Filter, FILTER_LENGTH * sizeof(PixelScalar), cudaMemcpyHostToDevice);
+    CUDA_CHECK_LAST_ERROR();
+    cudaMemcpy(d_Input, h_Input, imageW * imageH * sizeof(PixelScalar), cudaMemcpyHostToDevice);
+    CUDA_CHECK_LAST_ERROR();
 
     // to 'h_Filter' apotelei to filtro me to opoio ginetai to convolution kai
     // arxikopoieitai tuxaia. To 'h_Input' einai h eikona panw sthn opoia ginetai
@@ -133,13 +164,20 @@ int main(int argc, char **argv) {
     for (i = 0; i < imageW * imageH; i++) {
         h_Input[i] = (PixelScalar)rand() / ((PixelScalar)RAND_MAX / 255) + (PixelScalar)rand() / (PixelScalar)RAND_MAX;
     }
-    // TODO: To parakatw einai to kommati pou ekteleitai sthn CPU kai me vash auto prepei na ginei h sugrish me thn GPU.
+    
+    printf("GPU computation...\n");
+    // TODO: Launch kernel
 
+    // To parakatw einai to kommati pou ekteleitai sthn CPU kai me vash auto prepei na ginei h sugrish me thn GPU.
     printf("CPU computation...\n");
-
     convolutionRowCPU(h_Buffer, h_Input, h_Filter, imageW, imageH, filter_radius);
     convolutionColumnCPU(h_OutputCPU, h_Buffer, h_Filter, imageW, imageH, filter_radius);
-    
+
+    // After completing CPU computation wait for GPU and check for errors
+    CUDA_CHECK_LAST_ERROR();
+    cudaMemcpy(h_OutputGPU, d_Output, imageW * imageH * sizeof(PixelScalar), cudaMemcpyDeviceToHost);
+    CUDA_CHECK_LAST_ERROR();
+
     // TODO: Kanete h sugrish anamesa se GPU kai CPU kai an estw kai kapoio apotelesma xeperna thn akriveia
     // pou exoume orisei, tote exoume sfalma kai mporoume endexomenws na termatisoume to programma mas  
 
@@ -149,16 +187,40 @@ int main(int argc, char **argv) {
     //     }
     //     printf("\n");
     // }
+    
+    for (int y = 0; y < imageH; y++) {
+        for (int x = 0; x < imageW; x++) {
+            int index = y * imageW + x;
+
+            if (ABS(h_OutputCPU[index] - h_OutputGPU[index]) > accuracy) {
+                printf("Accuracy bigger than %f on pixel [%d, %d]\n", accuracy, x, y);
+                printf("  h_OutputCPU[%d]=%f\n", index, h_OutputCPU[index]);
+                printf("  h_OutputGPU[%d]=%f\n", index, h_OutputGPU[index]);
+                correctOutput = 0;
+                break;
+            }
+        }
+
+        if (!correctOutput)
+            break;
+    }
+
+    if (correctOutput)
+        printf("Results correct!\n");
 
     // Cleanup sequence
+    CLEANUP_DEVICE:
+    cudaFree(d_Filter);
+    cudaFree(d_Input);
+    cudaFree(d_Output);
     CLEANUP_HOST:
-    free(h_OutputCPU);
-    free(h_Buffer);
-    free(h_Input);
     free(h_Filter);
-    CLEANUP0:
-    // TODO: Do a device reset just in case... Bgalte to sxolio otan ylopoihsete CUDA
-    // cudaDeviceReset();
+    free(h_Input);
+    free(h_Buffer);
+    free(h_OutputCPU);
+
+    // Do a device reset just in case... Bgalte to sxolio otan ylopoihsete CUDA
+    cudaDeviceReset();
 
     return exitCode;
 }
