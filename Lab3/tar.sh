0516d05bf16b054f59f827269ae7e45523d06347
TsiantosD <tsiantos.d@gmail.com>
2025-11-25 23:13:15 +0200

    Add zero count plots and tar.sh

---
+++ Lab3/tar.sh
@@ -0,0 +1,11 @@
+tar -czvf NIKAS-IOANNIS-IASON_3771_TSOGKAS_PANAGIOTIS_3672_TSIANTOS_DIMITRIOS_3796.tar.gz \
+            src/ \
+            report.pdf \
+            run.sh \
+            5a-6a-precision.sh \
+            5b-6b-runtimes.sh \
+            8-runtimes.sh \
+            plot_precision_zeros.py \
+            plot_precision.py \
+            plot_runtimes.py \
+            requirements.txt
