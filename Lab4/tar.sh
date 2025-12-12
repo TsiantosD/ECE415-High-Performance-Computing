7505aec9dbf9291933ab05799de5b3d42948adf4
TsiantosD <tsiantos.d@gmail.com>
2025-12-13 00:00:14 +0200

    Add .tar.gz with submission, add new plots and tar.sh

---
+++ Lab4/tar.sh
@@ -0,0 +1,10 @@
+tar -czvf NIKAS-IOANNIS-IASON_3771_TSOGKAS_PANAGIOTIS_3672_TSIANTOS_DIMITRIOS_3796_lab4.tar.gz \
+            src/ \
+            run.sh \
+            gather_data.sh \
+            requirements.txt \
+            create_csv.py \
+            create_plot.py \
+            csv-senator \
+            csv-planet_surface \
+            report.pdf
